b63f57d1fe1284da5c44224df3cdc421fb26da89
Mara Manuel <maramnuel68@gmail.com.com>
2022-11-02 05:12:57 +0100

    Aprendido o básico de Sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import './css/style.css'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bola1">
+        <a href="#">Link</a>
+      </div>
+      <div className="bola2"></div>
+      <div className="bola3"></div>
     </div>
   );
 }
